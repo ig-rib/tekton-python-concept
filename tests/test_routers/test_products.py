9f8a43d8b1a331b173e993decb530727e88e5c2a
ig-rib <igrib98@gmail.com>
2023-01-09 17:21:49 -0300

    All tests, all passing

--- tests/test_routers/test_products.py
+++ tests/test_routers/test_products.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 from api.data_access_objects.product import ProductDAO
 
@@ -117,11 +118,56 @@ def test_update_product_with_conflicting_name(client, db_session):
     response = client.patch(f"/products/{product_2.id}", content=json.dumps(editable_data))
     assert response.status_code == 422
 
-# def test_update_products_with_bad_fields(clients):
-#     assert True
+def test_update_products_with_bad_fields(client, db_session):
+    product_1 = ProductDAO(
+        name= "TV",
+        status= 0,
+        stock= 54,
+        description= "Simple appliance",
+        price= 100000
+    )
+    db_session.add(product_1)
+    db_session.commit()
+    db_session.refresh(product_1)
+    product_2 = ProductDAO(
+        name= "Big TV",
+        status= 0,
+        stock= 54,
+        description= "Big simple appliance",
+        price= 150000
+    )
+    db_session.add(product_2)
+    db_session.commit()
+    db_session.refresh(product_2)
 
-# def test_get_product(clients):
-#     assert True
+    product_with_non_modifiable_fields = {
+        "id": product_2.id
+    }
+    response_1 = client.patch(f"/products/{product_1.id}",content=json.dumps(product_with_non_modifiable_fields))
+    assert response_1.json()["id"] == product_1.id
 
-# def test_get_non_existent_product(client):
-#     assert True
+def test_get_product(client, db_session):
+    product_1 = ProductDAO(
+        name= "TV",
+        status= 0,
+        stock= 54,
+        description= "Simple appliance",
+        price= 100000
+    )
+    db_session.add(product_1)
+    db_session.commit()
+    db_session.refresh(product_1)
+
+    response = client.get(f'/products/{product_1.id}')
+
+    assert response.json()["id"] == product_1.id
+    assert response.json()["name"] == product_1.name
+    assert response.json()["status"] == 'Inactive' if product_1.status == 0 else 'Active'
+    assert response.json()["stock"] == product_1.stock
+    assert response.json()["description"] == product_1.description
+    assert response.json()["price"] == product_1.price
+    
+
+def test_get_non_existent_product(client):
+    response = client.get(f'/products/1')
+    assert response.status_code == 404
